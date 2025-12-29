c7372f58c468c6a9fa23b5b64ea10d74271900c1
Ernst Schwaiger <ernst.schwaiger@chello.at>
2025-12-29 02:54:54 -0500

    add sequence number to manifest file

--- BuildSystem/FWUpdate/generator.sh
+++ BuildSystem/FWUpdate/generator.sh
@@ -9,25 +9,26 @@ FILE=$1
 MANIFEST=$2
 DATE=$(date +"%y%m%d-%H:%M")
 
+true > "${FILE}"
 {
   echo "__attribute__((section(\".hellofunc\")))"
   echo "void hellofunc(char *buf)"
   echo "{"
 
   for (( i=0; i<${#MSG}; i++ )); do
-    echo -n "    buf[$i] = '"
-    echo -n "${MSG:$i:1}"
-    echo "';"
+    printf "    buf[%d] = '%c';\n" "$i" "${MSG:$i:1}"
   done
-    echo "    buf[${#MSG}] = '\n';"
-    echo "    buf[$((${#MSG} + 1))] = '\0';"
 
+  printf "    buf[%d] = '\\\\n';\n" "${#MSG}"
+  printf "    buf[%d] = '\\\\0';\n" "$((${#MSG} + 1))"
   echo "}"
 }  >> "${FILE}"
 
+true > "${MANIFEST}"
 {
   echo "SRC_FILE=${FILE}"
   echo "VERSION=${VERSION}"
   echo "DATE=${DATE}"
+  echo "SEQUENCE_NUMBER=${SEQUENCE_NUMBER}"
 } >> "${MANIFEST}"
 
